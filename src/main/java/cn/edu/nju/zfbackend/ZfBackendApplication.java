c5f3d660708fc7ed0b04499ba0608f973794a5ee
shelton <lixiao1k@163.com>
2019-12-25 10:23:30 +0800

    first commit

---
+++ src/main/java/cn/edu/nju/zfbackend/ZfBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.zfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZfBackendApplication.class, args);
+    }
+
+}
